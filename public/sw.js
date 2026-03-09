f2b36c6acbcb6ae315f742b8a8ed49066110d6ef
NotSondre <seholm@uia.com>
2026-03-09 05:25:48 +0100

    Fikset opp i sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,25 @@
-const CACHE_NAME = 'pokeguess-cache-v1';
+const CACHE_NAME = 'pokeguess-cache-v2'; 
 const urlsToCache = [
   '/',
   '/index.html',
   '/app.js',
-  '/app.css',
-  '/manifest.json',
-  '/Terms%20Of%20Service%20-%20PokeGuesser.pdf',
-  '/Privacy%20Policy%20-%20PokeGuesser.pdf'
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caching assets...');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
