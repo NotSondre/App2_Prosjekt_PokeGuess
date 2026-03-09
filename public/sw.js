37afd942f43715f4190f35214e083009ccb54e87
NotSondre <seholm@uia.com>
2026-03-09 05:29:00 +0100

    Debugging ved gjetting av pokemon

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pokeguess-cache-v2';
+const CACHE_NAME = 'pokeguess-cache-v3';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -8,19 +8,18 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Caching assets...');
-        return Promise.all(
-          urlsToCache.map(url => {
-            return cache.add(url).catch(err => console.error('Feilet ved caching av:', url, err));
-          })
-        );
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  if (url.pathname.startsWith('/content') || url.pathname.startsWith('/user') || url.pathname.startsWith('/status')) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
