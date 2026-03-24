4579757372dacb2ae38e721d831367561319c038
NotSondre <seholm@uia.com>
2026-03-24 23:38:06 +0100

    Console log databasefeil

--- routes/user.mjs
+++ routes/user.mjs
@@ -78,7 +78,7 @@ router.post('/register', async (req, res) => {
         if (err.code === '23505') {
             return res.status(400).json({ error: "Brukernavnet er allerede tatt." });
         }
-        console.error("Databasefeil ved registrering:", err.message);
+        console.error("Databasefeil ved registrering:", err.message, err.stack);
         res.status(500).json({ error: "Serverfeil: Kunne ikke opprette bruker i databasen." });
     }
 });
